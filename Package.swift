b675796a3df4ef43f03f394db716c736abfb8392
eugene-gilmore <eugene.gilmore@griffithuni.edu.au>
2017-06-15 00:47:19 +1000

    Point to my fork of gtk module map

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 #if os(Linux)
-let dependencies:[Package.Dependency] = [.Package(url: "https://github.com/sclukey/cgtk3.git", majorVersion: 1)]
+let dependencies:[Package.Dependency] = [.Package(url: "https://github.com/eugene-gilmore/cgtk3.git", majorVersion: 1)]
 #else
 let dependencies:[Package.Dependency] = []
 #endif
